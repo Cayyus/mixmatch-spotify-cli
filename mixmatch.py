40f6d4a899e28c59334ad6083506daa4d2cbf10e
Caius <117181766+Cayyus@users.noreply.github.com>
2024-02-21 14:19:40 +0600

    Update mixmatch.py

--- mixmatch.py
+++ mixmatch.py
@@ -1,12 +1,19 @@
 from argparse import ArgumentParser, Namespace
-from parsing import print_albums, username, parse_print_playlists
-from track_paginator import st_wrap
 import os
 
+from parsers.parser import username
+from parsers.album_parsers import print_albums
+from parsers.playlist_parsers import parse_featured, parse_print_playlists
+from tables.track_paginator import st_wrap
+from tables.featured_tracktable import fs_wrap
+
 parser = ArgumentParser()
-parser.add_argument('-al', '--albums', nargs='?', const='', help='Print out your saved albums or a specific album if a name is provided')
+parser.add_argument('-al', '--albums', nargs='?', const='', 
+                    help='Print out your saved albums or a specific album if a name is provided')
 parser.add_argument('-pl', '--playlists', help='Print out your saved playlists', action='store_true')
 parser.add_argument('-st', '--liked_songs', help="See all your liked songs", action='store_true')
+parser.add_argument('-fs', '--featured_playlists', help='Discover playlists featured by Spotify', 
+                    nargs="?", const='')
 
 args: Namespace = parser.parse_args()
 
@@ -28,3 +35,11 @@ if args.playlists:
 if args.liked_songs:
   os.system('cls')
   st_wrap()
+
+if args.featured_playlists is not None:
+    os.system('cls')
+    if args.featured_playlists != '':
+        fs_wrap(args.featured_playlists)
+    else:
+        parse_featured()
+
