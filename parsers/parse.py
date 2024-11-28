8be3e6a108d9c20db39fedb92aabb5c23434fb36
Caius <117181766+Cayyus@users.noreply.github.com>
2024-11-28 12:06:33 +0600

    Update parse.py

--- parsers/parse.py
+++ parsers/parse.py
@@ -1,8 +1,10 @@
 from api.getapi import SpotifyGET
 from api.postapi import SpotifyPOST
+from api.putapi import SpotifyPUT
 
 api = SpotifyGET()
 papi = SpotifyPOST()
+puapi = SpotifyPUT()
 
 def hyperlink(text, url) -> str:
     return f'\033]8;;{url}\033\\{text}\033]8;;\033\\'
